daf6c2497de1a00bd9df05ca25107be9a5aa0558
Luke Olney <LukeOlney2016@u.northwestern.edu>
2015-04-09 12:19:21 -0500

    added update loop

--- app/example/scripts/controller.js
+++ app/example/scripts/controller.js
@@ -1,21 +1,20 @@
 angular.module('example', [])
 
-.controller('list', function($scope, $http) {
+.controller('list', function($scope, $http, $interval) {
   $scope.list = [];
 
   $scope.vote = function(item, direction){
     item.upvotes += direction * 1;
     $http.put('https://dry-coast-1630.herokuapp.com/post/' + item._id, {'upvotes':item['upvotes']})
-      .success(function (data, status, header, config){
-        
-      })
+      .success(function (data, status, header, config){})
       .error(function (response){
         item.text = response;
         supersonic.logger.debug(response);
       });
-  };
+    };
 
-  $http.get('https://dry-coast-1630.herokuapp.com/posts')
+  $scope.init = function(){
+    $http.get('https://dry-coast-1630.herokuapp.com/posts')
       .success(function (data, status, header, config) {
         supersonic.logger.debug(data);
       // response.data.children.forEach (function (entry, i) {
@@ -25,7 +24,16 @@ angular.module('example', [])
       //   $scope.list.push(tmp);
       // });
       $scope.list = data;
+      $scope.$apply();
     }).error(function (response) {
       supersonic.logger.debug(response);
     });
+  };
+
+  $scope.update = function(){
+    $scope.init();
+  };
+
+  $interval($scope.update, 1000);
+
 });
