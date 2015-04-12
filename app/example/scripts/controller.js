8b056ce651c3acdb796116378d97f7d3031909ec
Luke Olney <LukeOlney2016@u.northwestern.edu>
2015-04-12 14:18:31 -0500

    pagination/upvote limiting/pull to refresh now working

--- app/example/scripts/controller.js
+++ app/example/scripts/controller.js
@@ -3,10 +3,12 @@ angular.module('example', [])
 .controller('list', function($scope, $http, $interval) {
   $scope.list = [];
   $scope.pages = 0;
+  $scope.loading = false;
 
   $scope.vote = function(item, direction){
-    if(item.voteStatus == direction)
+    if(item.voteStatus == direction){
       return;
+    }
 
     item.upvotes += direction * 1;
     item.voteStatus = direction;
@@ -33,6 +35,10 @@ angular.module('example', [])
         console.log(response);
       });
     }); */
+    if($scope.loading){
+      return;
+    }
+    $scope.loading = true;
     
     var old_pages = $scope.pages;
     var old_list = {};
@@ -41,33 +47,50 @@ angular.module('example', [])
     });
     $scope.pages = 0;
     $scope.list = [];
-    for(var i=0; i<old_pages; i++){
-      $scope.fetchPage();
-    }
+
+    var onsuccess = function(data) {
+      data.forEach(function(item, i, array){
+        var voteStatus = old_list[item._id];
+        if(voteStatus){
+          console.log("text: " + item.text);
+          item.voteStatus = voteStatus;
+        } else{
+          console.log(old_list);
+        }
+      });
+    };
+    
+    $scope.fetchPage(old_pages, onsuccess);
     // this part won't be necessary if doing checking server-side:
-    $scope.list.forEach(function (item, i , array){
-      var voteStatus = old_list[item._id];
-      if(voteStatus){
-        item.voteStatus = voteStatus;
-      }
-    });
+    
     
   };
 
-  $scope.fetchPage = function(){ 
+  $scope.fetchPage = function(numpages, onsuccess){
+    $scope.loading = true;
     $http.get('https://dry-coast-1630.herokuapp.com/posts/' + $scope.pages)
       .success(function (data, status, header, config) {
+
+        $scope.pages += 1;
+        console.log("Current pages: " + $scope.pages);
         
         data.forEach(function(elem, i, array) {
           elem.voteStatus = 0;
           $scope.list.push(elem);
         });
 
-        $scope.pages += 0;
-        console.log($scope.pages);
+        if(onsuccess){
+          onsuccess(data);
+        }
+
+        $scope.loading = false;
+        if(numpages > 1){
+          fetchPage(numpages - 1, onsuccess);
+        }
 
       }).error(function (response) {
         console.log(response);
+        $scope.loading = false;
       });
  
   };
@@ -78,17 +101,18 @@ angular.module('example', [])
 })
 
 // Need to make sure this only gets called once
+// change "scroll" to "touchend"?
 .directive("scroll", function ($window, $document) {
     return function(scope, element, attrs) {
       angular.element($window).bind("scroll", function(){
         var height = $document[0].body.offsetHeight - this.innerHeight;
         // load next page
-         if (this.pageYOffset >= height) {
-            scope.fetchPage();
+         if (this.pageYOffset >= height && !scope.loading) {
+            scope.fetchPage(1, undefined);
             console.log(this.pageYOffset + " " + height);
          }
          // pull to refresh
-         else if (this.pageYOffset <= 0){
+         else if (this.pageYOffset <= 0 && !scope.loading){
             scope.update();
             console.log("not " + this.pageYOffset + " " + height);
          }
