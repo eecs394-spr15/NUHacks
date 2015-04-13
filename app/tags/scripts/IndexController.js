1eafc1d76ceeea7b2cc0a15ac44918ddb9ceb3e1
chenzhe142 <chenzhe142@163.com>
2015-04-12 19:45:23 -0500

    add listview to tags view

--- app/tags/scripts/IndexController.js
+++ app/tags/scripts/IndexController.js
@@ -1,14 +1,22 @@
-angular
-  .module('tags')
-  .controller('IndexController', function($scope, supersonic) {
+angular.module('tags', [])
+
+.controller('tag_list', function($scope, $http) {
     // Controller functionality here
-    $scope.callRestService= function() {
-  		$http({method: 'GET', url: '/someUrl'}).
-  			success(function(data, status, headers, config) {
-        		$scope.results.push(data);  //retrieve results and add to existing results
-    		})
-		}
-  });
+
+
+    $http.get("http://www.w3schools.com/angular/customers.php")
+    .success(function (response) {
+    	$scope.tag_list = response.records;
+    });
+
+
+  //   $scope.callRestService= function() {
+  // 		$http({method: 'GET', url: '/someUrl'}).
+  // 			success(function(data, status, headers, config) {
+  //       		$scope.results.push(data);  //retrieve results and add to existing results
+  //   		})
+		// }
+});
 
 app.directive('myOnKeyDownCall', function () {
     return function (scope, element, attrs) {
