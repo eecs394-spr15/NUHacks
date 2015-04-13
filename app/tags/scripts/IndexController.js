ba97c8f9a3b51114f89d3ec2bbb3133c9d84329e
chenzhe142 <chenzhe142@163.com>
2015-04-12 19:26:14 -0500

    add controller for tags module

--- app/tags/scripts/IndexController.js
+++ app/tags/scripts/IndexController.js
@@ -2,4 +2,21 @@ angular
   .module('tags')
   .controller('IndexController', function($scope, supersonic) {
     // Controller functionality here
+    $scope.callRestService= function() {
+  		$http({method: 'GET', url: '/someUrl'}).
+  			success(function(data, status, headers, config) {
+        		$scope.results.push(data);  //retrieve results and add to existing results
+    		})
+		}
   });
+
+app.directive('myOnKeyDownCall', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {            
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+        });
+    };
+});
