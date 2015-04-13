63c27dd81066ea3e673d4fa976e0f71e86404d87
chenzhe142 <chenzhe142@163.com>
2015-04-12 19:57:32 -0500

    delete test code, add comment for tags module

--- app/tags/scripts/IndexController.js
+++ app/tags/scripts/IndexController.js
@@ -3,28 +3,10 @@ angular.module('tags', [])
 .controller('tag_list', function($scope, $http) {
     // Controller functionality here
 
-
+    //todo: change this url to backend.
+    //this is test data from w3school.
     $http.get("http://www.w3schools.com/angular/customers.php")
     .success(function (response) {
     	$scope.tag_list = response.records;
     });
-
-
-  //   $scope.callRestService= function() {
-  // 		$http({method: 'GET', url: '/someUrl'}).
-  // 			success(function(data, status, headers, config) {
-  //       		$scope.results.push(data);  //retrieve results and add to existing results
-  //   		})
-		// }
-});
-
-app.directive('myOnKeyDownCall', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {            
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
-                event.preventDefault();
-        });
-    };
 });
