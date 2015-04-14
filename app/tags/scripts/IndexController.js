0667109b3fb695e8ec9ac43af63391bca7caac43
chenzhe142 <chenzhe142@163.com>
2015-04-13 19:07:43 -0500

    connect to database and show tags on tags view

--- app/tags/scripts/IndexController.js
+++ app/tags/scripts/IndexController.js
@@ -3,10 +3,40 @@ angular.module('tags', [])
 .controller('tag_list', function($scope, $http) {
     // Controller functionality here
 
-    //todo: change this url to backend.
-    //this is test data from w3school.
-    $http.get("http://www.w3schools.com/angular/customers.php")
+    $http.get("https://dry-coast-1630.herokuapp.com/posts/0/1")
     .success(function (response) {
-    	$scope.tag_list = response.records;
+    	$scope.tag_list = angular.fromJson(response);
     });
 });
+
+// .config(function($routeProvider) {
+// 	$routeProvider.
+//     when('/list', {
+//         controller: ListCtrl,
+//         templateUrl: 'list.html'
+//     }).
+//         when('/detail/:tag', {
+//         controller: DetailCtrl,
+//         templateUrl: 'detail.html'
+//     }).
+//     otherwise({
+//         redirectTo: '/list'
+//     });
+// });
+
+
+// function ListCtrl($scope, $location) {
+//     $scope.items = data;
+    
+//     $scope.goto_detail = function(tag) {
+//         $location.url('/detail/' + tag);
+//     };
+// }
+
+// function DetailCtrl($scope, $location, $routeParams) {
+//     $scope.item = data[$routeParams.id];
+
+//     $scope.goto_list = function() {
+//         $location.url('/list');
+//     };
+// }
