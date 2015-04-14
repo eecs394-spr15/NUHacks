06537b58c93f4454d0c188a9cf5d0162fbd203d3
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-04-13 23:26:02 -0500

    added new module, posts being added to database

---
+++ app/insert/index.js
@@ -0,0 +1,4 @@
+angular.module('insert', [
+  // Declare any module-specific AngularJS dependencies here
+  'common'
+]);
---
+++ app/insert/scripts/InsertController.js
@@ -0,0 +1,24 @@
+angular
+  .module('insert')
+  .controller('InsertController', function($scope, $http, supersonic) {
+    steroids.logger.log('Hello there!');
+    $scope.submitPost = function() {
+      steroids.logger.log('Inside submitPost()');
+    	var user = {};
+    	user.text = $scope.newText;
+      user.author = 'User';
+      steroids.logger.log(user.text);
+      steroids.logger.log(user.author);
+    	// user['tags'] = $scope.newTags.split(' ');
+    	$http.post('https://dry-coast-1630.herokuapp.com/post', user).
+    		success(function(data, status, headers, config) {
+          steroids.logger.log('success');
+          steroids.logger.log(status);
+        }).
+        error(function(data, status, headers, config) {
+          steroids.logger.log('error occurred');
+          steroids.logger.log(status);
+        })
+
+    };
+  });
