5002b8c58907505c0a5726c2d3a52a51ac005956
chenzhe142 <chenzhe142@163.com>
2015-04-14 23:57:54 -0500

    add search button(will remove later) and show tag search result list

--- app/tags/scripts/IndexController.js
+++ app/tags/scripts/IndexController.js
@@ -3,11 +3,24 @@ angular.module('tags', [])
 .controller('tag_list', function($scope, $http) {
     // Controller functionality here
 
-    $http.get("https://dry-coast-1630.herokuapp.com/posts/0/1")
+
+
+    $http.get("https://dry-coast-1630.herokuapp.com/tags")
     .success(function (response) {
     	$scope.tag_list = angular.fromJson(response);
     });
+
+    
+
+    $scope.search = function() {
+    	$http.get("https://dry-coast-1630.herokuapp.com/search/" + $scope.input_tag)
+    	.success(function (response) {
+    		$scope.result = angular.fromJson(response);
+    	});
+		//$scope.result = "John";
+    }
 });
+
 
 // search by tags
 // https://dry-coast-1630.herokuapp.com/search/:tags
@@ -15,20 +28,4 @@ angular.module('tags', [])
 
 // todo: routing when clicking table rows,
 // 		 use templates
-
-
-// .config(function($routeProvider) {
-//     $routeProvider.
-//     when('/list', {
-//         controller: ListCtrl,
-//         templateUrl: 'list.html'
-//     }).
-//         when('/detail/:id', {
-//         controller: DetailCtrl,
-//         templateUrl: 'detail.html'
-//     }).
-//     otherwise({
-//         redirectTo: '/list'
-//     });
-// });
 
