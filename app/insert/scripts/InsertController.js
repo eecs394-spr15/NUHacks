8a2d7dd03c0a1496ab4b1b10c63281a38fdb488a
Luke Olney <LukeOlney2016@u.northwestern.edu>
2015-04-15 17:27:08 -0500

    go back on post submit

--- app/insert/scripts/InsertController.js
+++ app/insert/scripts/InsertController.js
@@ -2,18 +2,17 @@ angular
   .module('insert')
   .controller('InsertController', function($scope, $http, supersonic) {
     steroids.logger.log('Hello there!');
-    $scope.submitPost = function() {
+    $scope.submitPost = function(post) {
       steroids.logger.log('Inside submitPost()');
-    	var user = {};
-    	user.text = $scope.newText;
-      user.author = 'User';
-      steroids.logger.log(user.text);
-      steroids.logger.log(user.author);
+      post.author = 'User';
+      steroids.logger.log(post.text);
+      steroids.logger.log(post.author);
     	// user['tags'] = $scope.newTags.split(' ');
-    	$http.post('https://dry-coast-1630.herokuapp.com/post', user).
+    	$http.post('https://dry-coast-1630.herokuapp.com/post', post).
     		success(function(data, status, headers, config) {
           steroids.logger.log('success');
           steroids.logger.log(status);
+          supersonic.ui.layers.pop();
         }).
         error(function(data, status, headers, config) {
           steroids.logger.log('error occurred');
