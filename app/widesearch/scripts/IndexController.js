ebaec50701ab59d18e59cae20b4fbfe423f8f7b0
Guandakd35 <leegd313@gmail.com>
2015-04-21 16:50:05 -0500

    updating my module

--- app/widesearch/scripts/IndexController.js
+++ app/widesearch/scripts/IndexController.js
@@ -1,5 +1,26 @@
 angular
-  .module('widesearch')
-  .controller('IndexController', function($scope, supersonic) {
-    // Controller functionality here
+  .module('searchhack',[])
+  .controller('searchpage', function($scope,$http) {
+  	//$scope.searchpage = [];
+  	$scope.xx=0;
+  	$scope.isxx=true;
+    // Controller functionality here`
+    $scope.search = function(){
+    	$http.get("https://dry-coast-1630.herokuapp.com/search_hack/" + $scope.hack_need)
+    	.success(function(response){
+    		$scope.searchpage = angular.fromJson(response);
+    		// $scope.xx=1;
+      		});
+
+
+    	});
+    }
+    $scope.isxxx = function(){
+    	if ($scope.xx >1)
+    		return true;
+    	else
+    		return false;
+    	// $scope.xx=0
+    };
+    
   });
